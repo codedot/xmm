f938d83f5776e027de47f0e39c5aeacab408564a
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2016-10-04 11:27:30 +0300

    Define default configuration filename

--- cli.js
+++ cli.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 
+const home = require("os").homedir();
+const conf = require("path").join(home, ".xmm.json");
 const argv = require("yargs")
-	.usage("Usage: $0 command [options]")
+	.usage("Usage: $0 [options] [command [arguments]]")
+	.config()
+	.alias("config", "c")
+	.global("config")
+	.default("config", conf)
 	.command(require("./ledger"))
-	.demand(1)
-	.strict()
+	.version()
+	.alias("version", "v")
 	.help()
+	.alias("help", "h")
 	.argv;
 
 console.log(argv);
