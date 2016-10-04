650f1af6c01c733ef80e967e415a36c6122d5597
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2016-10-04 15:29:33 +0300

    Define --server and --delta options

--- cli.js
+++ cli.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
 
+const opts = {
+	server: {
+		alias: "s",
+		describe: "WebSocket server URL",
+		default: "wss://s1.ripple.com",
+		global: true
+	},
+	delta: {
+		alias: "d",
+		describe: "Stake to trade",
+		default: 0.01,
+		global: true
+	}
+};
+
 const home = require("os").homedir();
 const conf = require("path").join(home, ".xmm.json");
 const argv = require("yargs")
 	.usage("Usage: $0 [options] [command [arguments]]")
+	.options(opts)
 	.config()
 	.alias("config", "c")
 	.global("config")
