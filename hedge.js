8566e289e507eecbd478275ad7eee473ab1351b1
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2017-03-19 04:20:48 +0200

    Filter relevant offers in "hedge"

--- hedge.js
+++ hedge.js
@@ -6,6 +6,52 @@ exports.aliases = [
 ];
 exports.builder = yargs => yargs;
 exports.handler = connect((config, xmm) => {
-	console.info(xmm.ledger);
-	process.exit();
+	const me = config.me;
+	const ledger = config.ledger;
+
+	Promise.all([
+		xmm.balance(me, ledger),
+		xmm.offers(me, ledger)
+	]).then(state => {
+		const assets = {};
+		const xrp = `XRP@${me}`;
+
+		state[0].forEach(line => {
+			const value = line.value;
+
+			if (value > 0) {
+				const asset = xmm.parse({
+					type: "asset",
+					asset: line.asset,
+					wallet: me
+				}).human;
+
+				assets[asset] = value;
+				return true;
+			}
+		});
+
+		state[1].filter(line => {
+			const src = xmm.parse({
+				type: "asset",
+				asset: line.base,
+				wallet: me
+			}).human;
+			const dst = xmm.parse({
+				type: "asset",
+				asset: line.asset,
+				wallet: me
+			}).human;
+
+			if (!assets[src] || !assets[dst])
+				return false;
+
+			if ((xrp == src) || (xrp == dst))
+				return true;
+		}).forEach(line => {
+			console.log(line.human);
+		});
+
+		process.exit();
+	}).catch(abort);
 });
