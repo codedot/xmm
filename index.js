c6860f6feb826b598ad64314539cd178bc12acdc
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2017-04-24 23:53:01 +0300

    Work around expired certificates of testnet

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const faucet = "https://faucet.altnet.rippletest.net/accounts";
-const altnet = "wss://s.altnet.rippletest.net:51233";
+const faucet = "http://faucet.altnet.rippletest.net/accounts";
+const altnet = "ws://s.altnet.rippletest.net:51233";
 const root = "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh";
 const syntax = "^(?:_(?::_(?:\\\/_:_~_)?)?@)?_$";
 const pattern = syntax.replace(/_/g, "([^:@\\\/~]+)");
