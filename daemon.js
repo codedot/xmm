79f9955906c8ae6eb089e4a986d7b35002ec6621
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2018-02-26 03:20:59 +0200

    Generate secp256k1

--- daemon.js
+++ daemon.js
@@ -1,3 +1,7 @@
 "use strict";
 
-console.info("Hello World!");
+const ecdh = require("crypto").createECDH("secp256k1");
+const pub = ecdh.generateKeys("base64", "compressed");
+const priv = ecdh.getPrivateKey("base64");
+
+console.info(pub, priv);
