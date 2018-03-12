2a68ba74712f39d3f71169b97943ca841d558104
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2018-03-13 01:23:00 +0200

    Factor out key.js

--- daemon.js
+++ daemon.js
@@ -1,45 +1,16 @@
 "use strict";
 
-const assert = require("assert");
-const crypto = require("crypto");
-const bs58 = require("bs58");
-const secp256k1 = require("secp256k1");
-const ecdh = crypto.createECDH("secp256k1");
-const pub = ecdh.generateKeys("hex", "compressed");
-const priv = ecdh.getPrivateKey();
-const pub2 = secp256k1.publicKeyCreate(priv);
-const sha512 = data => {
-	const hash = crypto.createHash("sha512");
+const key = require("./key");
 
-	hash.update(data);
-	return hash.digest();
-};
-const xor = (a, b) => {
-	const x = [];
-	const n = a.length;
-
-	assert.equal(a.length, b.length);
-
-	for (let i = 0; i < n; i++)
-		x.push(a[i] ^ b[i]);
-
-	return Buffer.from(x);
-};
 const socket = require("tls").connect({
 	host: "r.ripple.com",
 	port: 51235
 }, () => {
-	const client = socket.getFinished().toString("hex");
-	const server = socket.getPeerFinished().toString("hex");
-	const mix = xor(sha512(client), sha512(server));
-	const shared = sha512(mix).slice(0, 32);
-	const sig = secp256k1.sign(shared, priv).signature;
-	const base64 = sig.toString("base64");
+	const sig = key.sign(socket).toString("base64");
 
-	console.log(`Session-Signature: ${base64}`);
+	console.info(`Session-Signature: ${sig}`);
+
 	socket.end();
 });
 
-assert(secp256k1.privateKeyVerify(priv));
-
-console.info(`Public-Key: ${bs58.encode(pub2)}`);
+console.info(`Public-Key: ${key.pub}`);
---
+++ key.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const assert = require("assert");
+const crypto = require("crypto");
+const bs58 = require("bs58");
+const secp256k1 = require("secp256k1");
+
+const ecdh = crypto.createECDH("secp256k1");
+let key;
+
+function sha512(data)
+{
+	const hash = crypto.createHash("sha512");
+
+	hash.update(data);
+	return hash.digest();
+};
+
+function xor(a, b)
+{
+	const x = [];
+	const n = a.length;
+
+	assert.equal(a.length, b.length);
+
+	for (let i = 0; i < n; i++)
+		x.push(a[i] ^ b[i]);
+
+	return Buffer.from(x);
+}
+
+function sign(socket)
+{
+	const client = socket.getFinished();
+	const server = socket.getPeerFinished();
+	const mix = xor(sha512(client), sha512(server));
+	const shared = sha512(mix).slice(0, 32);
+
+	return secp256k1.sign(shared, key).signature;
+}
+
+ecdh.generateKeys();
+key = ecdh.getPrivateKey();
+assert(secp256k1.privateKeyVerify(key));
+
+exports.sign = sign;
+exports.pub = bs58.encode(secp256k1.publicKeyCreate(key));
