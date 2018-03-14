0c7c46744b0404aed63f9489e53750c945759205
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2018-03-14 16:14:21 +0200

    Factor out connect.js

---
+++ connect.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const key = require("./key");
+
+const https = require("https");
+
+const consume = msg => new Promise(resolve => {
+	const chunks = [];
+
+	msg.setEncoding("utf8");
+	msg.on("data", chunk => {
+		chunks.push(chunk);
+	});
+	msg.on("end", () => {
+		resolve(chunks.join(""));
+	});
+});
+
+module.exports = peer => new Promise((resolve, reject) => {
+	const address = peer.split(":");
+	const host = address.shift();
+	const port = parseInt(address.shift());
+	const req = https.request({
+		hostname: host,
+		port: port,
+		headers: {
+			"Upgrade": "RTXP/1.2",
+			"Connection": "Upgrade"
+		},
+		rejectUnauthorized: false,
+		timeout: 5e3
+	}, msg => {
+		const status = msg.statusCode;
+		const headers = msg.headers;
+		const res = {
+			peer: peer,
+			status: status,
+			headers: headers
+		};
+
+		if (101 == status) {
+			resolve(res);
+			return;
+		}
+
+		consume(msg).then(body => {
+			res.body = body;
+			resolve(res);
+		}).catch(reject);
+	});
+
+	req.on("timeout", () => {
+		throw "Timeout";
+	});
+	req.on("socket", socket => {
+		socket.on("secureConnect", () => {
+			const proof = key.sign(socket);
+			const sig = proof.toString("base64");
+
+			req.setHeader("Public-Key", key.pub);
+			req.setHeader("Session-Signature", sig);
+			req.end();
+		});
+	});
+});
--- daemon.js
+++ daemon.js
@@ -1,44 +1,11 @@
 "use strict";
 
-const key = require("./key");
+const connect = require("./connect");
 
-const req = require("https").request({
-	hostname: "r.ripple.com",
-	port: 51235,
-	headers: {
-		"Upgrade": "RTXP/1.2",
-		"Connection": "Upgrade"
-	},
-	rejectUnauthorized: false,
-	timeout: 5e3
-}, res => {
-	const status = res.statusCode;
-
-	console.log(status, res.headers);
-
-	if (101 != status)
-		consume(res);
-});
-
-function consume(res)
-{
-	const chunks = [];
-
-	res.setEncoding("utf8");
-	res.on("data", chunk => {
-		chunks.push(chunk);
-	});
-	res.on("end", () => {
-		console.log(chunks.join(""));
-	});
-}
-
-req.on("socket", socket => {
-	socket.on("secureConnect", () => {
-		const sig = key.sign(socket).toString("base64");
-
-		req.setHeader("Public-Key", key.pub);
-		req.setHeader("Session-Signature", sig);
-		req.end();
-	});
+connect("r.ripple.com:51235").then(res => {
+	console.info(res);
+	process.exit();
+}).catch(err => {
+	console.warn(err);
+	process.exit(1);
 });
