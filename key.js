1a469a19e1b197a08b7509fdb3a596165d463407
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2018-03-16 16:49:24 +0200

    Merge helper.js into key.js

--- key.js
+++ key.js
@@ -1,12 +1,38 @@
 "use strict";
 
-const helper = require("./helper");
+const basex = require("base-x");
+const crypto = require("crypto");
+const elliptic = require("elliptic");
 
-const base58 = helper.base58;
-const cksum = helper.cksum;
-const ec = helper.ec;
-const mkshared = helper.mkshared;
+const abc = "rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz";
+const sha = version => data => {
+        const hash = crypto.createHash("sha" + version);
 
+        return hash.update(data).digest();
+};
+const sha256 = sha("256");
+const sha512 = sha("512");
+const xor = (a, b) => {
+	const x = [];
+	const n = a.length;
+
+	for (let i = 0; i < n; i++)
+		x.push(a[i] ^ b[i]);
+
+	return Buffer.from(x);
+};
+const base58 = basex(abc);
+const ec = new elliptic.ec("secp256k1");
+const cksum = data => {
+        data = sha256(data);
+        data = sha256(data);
+        return data.slice(0, 4);
+};
+const mkshared = pair => {
+	const mix = xor(sha512(pair[0]), sha512(pair[1]));
+
+	return sha512(mix).slice(0, 32);
+};
 const secret = ec.genKeyPair();
 
 function derive()
