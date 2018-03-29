815c3b84160496290dbcd4b204cff1b357a077a4
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2018-03-30 02:34:36 +0300

    Print profit in bp and spread in %

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 all:
-	node zigzag.js ""
-	node zigzag.js 10
-	node zigzag.js 10,16
-	node zigzag.js 10,14,12,16
-	node zigzag.js 10,13,16
+	node zigzag ""
+	node zigzag 10
+	node zigzag 10,16
+	node zigzag 10,14,12,16
+	node zigzag 10,13,16
 
 clean:
--- zigzag.js
+++ zigzag.js
@@ -8,5 +8,15 @@ const arg = process.argv.pop();
 const json = `[${arg}]`;
 const prices = JSON.parse(json);
 const res = zigzag(prices);
+let profit = res.profit;
+let spread = res.spread;
 
-console.info(res);
+profit = 1e4 * (profit - 1);
+profit = profit.toFixed(2);
+profit = `${profit}bp`;
+console.info("profit", profit);
+
+spread = 100 * (spread - 1);
+spread = spread.toFixed(2);
+spread = `${spread}%`;
+console.info("spread", spread);
